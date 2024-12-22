1a5711dda98f1c0b78264e7ee45acdf527c049cf
HARSHAL SAHADEOKAR <131584127+harshal1996sahadeokar@users.noreply.github.com>
2024-12-22 16:10:16 +0530

    Create variables.tf

---
+++ Ansible_Terra_PingTest/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+#variable "ssh_public_key_path" {
+  #default = "~/.ssh/id_rsa.pub" # Path to your public SSH key
+#}
+
+#variable "ssh_private_key_path" {
+ # default = "~/.ssh/id_rsa" # Path to your private SSH key
+#}
+
